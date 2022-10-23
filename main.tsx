7ba32c4bf19d97230b530dab49180f77f6ea0565
Jesse Chounard <jessechounard@gmail.com>
2022-10-22 23:19:44 -0500

    Initial commit, blog exists

---
+++ main.tsx
@@ -0,0 +1,18 @@
+/** @jsx h */
+
+import blog from "blog";
+
+blog({
+  title: "Jesse's Inanity",
+  description: "It's a blog.",
+  author: "Jesse Chounard",
+  avatar: "./jesse.jpg",
+  avatarClass: "rounded-lg",
+  links: [
+    { title: "GitHub", url: "https://github.com/jessechounard" },
+    { title: "Twitter", url: "https://twitter.com/Chounard" },
+    { title: "Linkedin", url: "https://www.linkedin.com/in/jesse-chounard/" },
+    { title: "Email", url: "mailto:jessechounard@gmail.com" },
+  ],
+  theme: "dark",
+});
---
+++ posts/first_post.md
@@ -0,0 +1,8 @@
+---
+title: First post
+publish_date: 2022-10-22
+---
+
+You have encountered my blog. If you have reached this destination by mistake, please enjoy this [photo of a kitten](./kitten.jpg), and have a nice day.
+
+If you're here on purpose, well... I guess you can still go look at the kitten.
